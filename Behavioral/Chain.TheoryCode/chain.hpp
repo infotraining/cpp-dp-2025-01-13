bfac25357850cdcc43b900d095ac26be071fb956
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-01-15 14:41:32 +0100

    Refactor Handler class to improve request handling logic and encapsulate request processing

--- Behavioral/Chain.TheoryCode/chain.hpp
+++ Behavioral/Chain.TheoryCode/chain.hpp
@@ -8,9 +8,7 @@
 // "Handler"
 class Handler
 {
-protected:
     std::shared_ptr<Handler> successor_;
-
 public:
     Handler() : successor_{nullptr} {}
 
@@ -19,47 +17,70 @@ public:
         successor_ = successor;
     }
 
-    virtual void handle_request(int request) = 0;
+    void handle_request(int request)
+    {
+        bool is_accepted = false;
+
+        if (can_handle(request))
+            process_request(request, is_accepted);
+
+        if (is_accepted)
+            return;
+
+        if (successor_ != nullptr)
+            successor_->handle_request(request);
+    }
 
     virtual ~Handler() = default;
+protected:
+    virtual bool can_handle(int request) = 0;
+    virtual void process_request(int request, bool& is_accepted) = 0;
 };
 
 // "ConcreteHandler1"
 class ConcreteHandler1 : public Handler
 {
-public:
-    void handle_request(int request)
+protected:
+    bool can_handle(int request) override
     {
-        if ((request >= 0) && (request < 10))
-            std::cout << "ConcreteHandler1 handled request " << request << std::endl;
-        else if (successor_ != nullptr)
-            successor_->handle_request(request);
+        return (request >= 0) && (request < 10);
+    }
+
+    void process_request(int request, bool& is_accepted) override
+    {
+        std::cout << "ConcreteHandler1 handled request " << request << std::endl;
     }
 };
 
 // "ConcreteHandler2"
 class ConcreteHandler2 : public Handler
 {
-public:
-    void handle_request(int request)
+protected:
+    bool can_handle(int request) override
     {
-        if ((request >= 10) && (request < 20))
-            std::cout << "ConcreteHandler2 handled request " << request << std::endl;
-        else if (successor_ != nullptr)
-            successor_->handle_request(request);
+        return (request >= 10) && (request < 20);
+    }
+
+    void process_request(int request, bool& is_accepted) override
+    {
+        std::cout << "ConcreteHandler2 handled request " << request << std::endl;
+
+        is_accepted = true;
     }
 };
 
 // "ConcreteHandler3"
 class ConcreteHandler3 : public Handler
 {
-public:
-    void handle_request(int request)
+protected:
+    bool can_handle(int request) override
     {
-        if ((request >= 20) && (request < 30))
-            std::cout << "ConcreteHandler3 handled request " << request << std::endl;
-        else if (successor_ != nullptr)
-            successor_->handle_request(request);
+        return (request >= 20) && (request < 30);
+    }
+
+    void process_request(int request, bool& is_accepted) override
+    {
+        std::cout << "ConcreteHandler3 handled request " << request << std::endl;
     }
 };
 
